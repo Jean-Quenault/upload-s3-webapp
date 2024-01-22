be032b7a76105f8607941044185158648bfdc999
Jean-Quenault <jeanform@proton.me>
2024-01-22 18:22:55 +0100

    Remove credentials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function UploadImageToS3() {
+  const [image, setImage] = useState(null);
+
+  const handleFileInput = (e) => {
+    setImage(e.target.files[0]);
+  }
+
+  const uploadFile = async (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async (e) => {
+      const base64 = e.target.result.split(',')[1];
+
+      const data = {
+        fileName: file.name,
+        fileContent: base64,
+        contentType: file.type
+      };
+
+      try {
+        const response = await fetch('URL_DE_VOTRE_API', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (error) {
+        console.error('Erreur lors de l\'upload:', error);
+      }
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleFileInput}/>
+      <button onClick={() => uploadFile(image)}>Upload to S3</button>
     </div>
   );
 }
 
-export default App;
+export default UploadImageToS3;
