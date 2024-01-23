e468ab007ebb2aa5bd05463cb4819c032ce153d3
Jean-Quenault <jeanform@proton.me>
2024-01-23 18:01:17 +0100

    Test with API

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-function UploadImageToS3() {
-  const [image, setImage] = useState(null);
+function ImageUpload() {
+  const [image, setImage] = useState('');
 
-  const handleFileInput = (e) => {
-    setImage(e.target.files[0]);
-  }
-
-  const uploadFile = async (file) => {
+  const handleImageChange = e => {
+    const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = async (e) => {
-      const base64 = e.target.result.split(',')[1];
-
-      const data = {
-        fileName: file.name,
-        fileContent: base64,
-        contentType: file.type
-      };
-
-      try {
-        const response = await fetch('URL_DE_VOTRE_API', {
-          method: 'POST',
-          body: JSON.stringify(data),
-          headers: { 'Content-Type': 'application/json' }
-        });
-        const responseData = await response.json();
-        console.log(responseData);
-      } catch (error) {
-        console.error('Erreur lors de l\'upload:', error);
-      }
+    reader.onloadend = () => {
+      setImage(reader.result);
     };
-  }
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://x1mavx5ya0.execute-api.eu-west-3.amazonaws.com/Dev/send', {
+        image: image.split(',')[1] // Supprime le préfixe de la chaîne base64
+      });
+      const downloadUrl = response.data.download_url;
+      window.open(downloadUrl, '_blank');
+    } catch (error) {
+      console.error('Erreur lors de l’envoi de l’image', error);
+    }
+  };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileInput}/>
-      <button onClick={() => uploadFile(image)}>Upload to S3</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleImageChange} />
+      <button type="submit">Convertir et Télécharger</button>
+    </form>
   );
 }
 
-export default UploadImageToS3;
+export default ImageUpload;
