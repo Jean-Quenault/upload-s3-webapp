e20e18e01d27a12624869e2152aa54cd10494de6
Jean-Quenault <jeanform@proton.me>
2024-01-23 19:17:16 +0100

    Test upload by request link from S3

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import axios from 'axios';
 
 function ImageUpload() {
   const [image, setImage] = useState('');
+  const [fileName, setFileName] = useState('');
 
   const handleImageChange = e => {
     const file = e.target.files[0];
+    setFileName(file.name); // Stocker le nom du fichier
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onloadend = () => {
-      setImage(reader.result.split(',')[1]); // Supprime le préfixe base64 ici
+      setImage(reader.result.split(',')[1]); // Supprime le préfixe base64
     };
   };
 
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://x1mavx5ya0.execute-api.eu-west-3.amazonaws.com/Dev/send', {
-        image: image // Utilisez l'image déjà transformée
+      const response = await axios.post('https://x1mavx5ya0.execute-api.eu-west-3.amazonaws.com/Dev/upload', image, {
+        headers: {
+          'Content-Type': 'application/octet-stream',
+          'file-name': fileName, // Inclure le nom du fichier dans les en-têtes
+        }
       });
-      const downloadUrl = response.data.download_url;
-      window.open(downloadUrl, '_blank');
+      console.log('Réponse de l’API:', response.data);
     } catch (error) {
       console.error('Erreur lors de l’envoi de l’image', error);
     }
@@ -29,7 +33,7 @@ function ImageUpload() {
   return (
     <form onSubmit={handleSubmit}>
       <input type="file" onChange={handleImageChange} />
-      <button type="submit">Convertir et Télécharger</button>
+      <button type="submit">Télécharger sur S3</button>
     </form>
   );
 }
