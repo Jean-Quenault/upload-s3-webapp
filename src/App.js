869d296b6e4c16b2de200c49e2f7db199ff7ac16
Jean-Quenault <jeanform@proton.me>
2024-01-23 19:47:35 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function ImageUpload() {
-  const [image, setImage] = useState('');
+  const [imageFile, setImageFile] = useState(null); // Stocker le fichier lui-même
   const [fileName, setFileName] = useState('');
 
   const handleImageChange = e => {
     const file = e.target.files[0];
-    setFileName(file.name); // Stocker le nom du fichier
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImage(reader.result.split(',')[1]); // Supprime le préfixe base64
-    };
+    setFileName(file.name);
+    setImageFile(file); // Stocker le fichier
   };
 
   const handleSubmit = async e => {
     e.preventDefault();
+    if (!imageFile) {
+      console.error("Aucun fichier sélectionné");
+      return;
+    }
+
     try {
-      const response = await axios.post('https://x1mavx5ya0.execute-api.eu-west-3.amazonaws.com/Dev/upload', image, {
+      // Obtenir l'URL présignée depuis l'API Gateway
+      const presignedResponse = await axios.get(`https://x1mavx5ya0.execute-api.eu-west-3.amazonaws.com/Dev/upload?file_name=${fileName}`);
+      const { url: presignedUrl } = presignedResponse.data;
+
+      // Utiliser l'URL présignée pour l'upload
+      await axios.put(presignedUrl, imageFile, {
         headers: {
-          'Content-Type': 'application/octet-stream',
-          'file-name': fileName, // Inclure le nom du fichier dans les en-têtes
+          'Content-Type': 'application/octet-stream'
         }
       });
-      console.log('Réponse de l’API:', response.data);
+      
+      console.log('Image téléchargée avec succès sur S3');
     } catch (error) {
       console.error('Erreur lors de l’envoi de l’image', error);
     }
