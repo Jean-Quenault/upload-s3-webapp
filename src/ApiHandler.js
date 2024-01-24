c8aaab98b0c000a40d88c070f2bee39828749bbe
Jean-Quenault <jeanform@proton.me>
2024-01-24 21:16:44 +0100

    Interface with @mui/material

--- src/ApiHandler.js
+++ src/ApiHandler.js
@@ -40,13 +40,12 @@ function ApiHandler() {
 
   // The component renders a file input and a button for uploading the file
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={uploadFile}>Envoyer le fichier</button>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', backgroundColor: '#f5f5f5' }}>
+      <h1 style={{ marginBottom: '50px' }}>Webapp to upload file on private S3</h1>
+      <input type="file" onChange={handleFileChange} style={{ marginBottom: '20px' }} />
+      <button onClick={uploadFile} style={{ padding: '10px 20px', backgroundColor: 'blue', color: 'white', border: 'none', borderRadius: '5px' }}>Envoyer le fichier</button>
     </div>
   );
 }
 
 export default ApiHandler;
-
-// Test
